8d7cee1f479ec01f2356c5cc4314e025132088e7
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 19:19:53 +0530

    Update nat_gateway.tf

--- Terraform/nat_gateway.tf
+++ Terraform/nat_gateway.tf
@@ -5,7 +5,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "nat" {
 
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_1.id
+  subnet_id     = aws_subnet.web_public_1a.id
 
   tags = {
     Name = "three-tier-nat"
