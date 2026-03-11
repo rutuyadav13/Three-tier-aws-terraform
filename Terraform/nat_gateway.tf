47d0cde2005605edd391be88e79da3e7b9272b60
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 19:04:18 +0530

    Update nat_gateway.tf

--- Terraform/nat_gateway.tf
+++ Terraform/nat_gateway.tf
@@ -1,9 +1,12 @@
 resource "aws_eip" "nat_eip" {
-
   domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
 
   tags = {
-    Name = "three-tier-nat-eip"
+    Name = "three-tier-nat"
   }
-
 }
