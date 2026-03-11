2234baaee9d0f72e9b8404753ef8b65f09f81692
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 19:29:44 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
