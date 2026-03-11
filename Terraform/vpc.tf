bd5373232241570273bbb51a80876d5d8d1971f3
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 19:03:57 +0530

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,6 +1,3 @@
-# -------------------------
-# VPC
-# -------------------------
 resource "aws_vpc" "three_tier_vpc" {
 
   cidr_block = var.vpc_cidr
@@ -12,42 +9,6 @@ resource "aws_vpc" "three_tier_vpc" {
     Name = "three-tier-vpc"
   }
 }
-
-# -------------------------
-# Internet Gateway
-# -------------------------
-resource "aws_internet_gateway" "igw" {
-
-  vpc_id = aws_vpc.three_tier_vpc.id
-
-  tags = {
-    Name = "three-tier-igw"
-  }
-}
-
-# -------------------------
-# Elastic IP for NAT
-# -------------------------
-resource "aws_eip" "nat_eip" {
-
-  domain = "vpc"
-
-  tags = {
-    Name = "three-tier-nat-eip"
-  }
-}
-
-# -------------------------
-# NAT Gateway
-# -------------------------
-resource "aws_nat_gateway" "nat" {
-
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_1.id
-
-  tags = {
-    Name = "three-tier-nat-gateway"
-  }
 
   depends_on = [
     aws_internet_gateway.igw
