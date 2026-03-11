066aa1c9bf2c3b907f1510187bdafd2db4789b56
Rutuja yadav <rutuyadav13998@gmail.com>
2026-03-11 19:15:35 +0530

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,23 +1,7 @@
-variable "aws_region" {
-  default = "ap-south-1"
+variable "region" {
+  default = "us-east-1"
 }
 
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
-}
-
-variable "public_subnet_1_cidr" {
-  default = "10.0.1.0/24"
-}
-
-variable "public_subnet_2_cidr" {
-  default = "10.0.2.0/24"
-}
-
-variable "private_subnet_1_cidr" {
-  default = "10.0.3.0/24"
-}
-
-variable "private_subnet_2_cidr" {
-  default = "10.0.4.0/24"
 }
